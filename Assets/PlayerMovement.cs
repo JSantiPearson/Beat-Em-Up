7cf441a0318553b4e2d3c63ab654035a32ebf5b4
Jordan Pearson <jspearson@pugetsound.edu>
2019-01-30 15:18:15 -0800

    Move in-progress game to git

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float maxSpeed = 6f;
+    bool facingRight = true;
+ 
+    Animator anim;
+ 
+    void Start ()
+    {
+        anim = GetComponent<Animator> ();
+    }
+ 
+    void FixedUpdate ()
+    {
+        float moveH = Input.GetAxisRaw ("Horizontal");
+        float moveV = Input.GetAxisRaw ("Vertical");
+ 
+        anim.SetFloat ("Speed", Mathf.Abs (moveH*moveV+(moveV+moveH)));
+ 
+        GetComponent<Rigidbody2D>().velocity = new Vector2 (moveH * maxSpeed, moveV * maxSpeed);
+ 
+        if (moveH > 0 && !facingRight)
+            Flip ();
+        else if (moveH < 0 && facingRight)
+            Flip ();
+    }
+ 
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 TheScale = transform.localScale;
+        TheScale.x *= -1;
+        transform.localScale = TheScale;
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# BEAT EM UP GAME
+
+## A work in progress beat 'em up, temporarily using assets from Scott Pilgrim and basing the game's structure off of Scott Pilgrim the Game. Eventually it'll have original assets.
